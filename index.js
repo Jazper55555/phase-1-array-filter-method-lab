07296ad0d1975d4b0c6aedc289fab7beac932732
Jazper <jazpersaldana@yahoo.com>
2023-10-28 15:59:33 -0500

    array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => {
+      return driver.toLowerCase() === name.toLowerCase();
+    });
+    return matchingDrivers;
+  }
+
+function fuzzyMatch(drivers, string) {
+    const matchingDrivers = drivers.filter(driver => driver.startsWith(string));
+    return matchingDrivers
+}
+
+
+function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => driver.name === name);
+        return matchingDrivers
+    }
